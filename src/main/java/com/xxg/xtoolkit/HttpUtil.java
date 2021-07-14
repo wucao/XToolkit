4047a066368729c87ef45972ed76e9da3d03faa0
吴操 <wucao@qiyi.com>
2021-07-14 14:46:10 +0800

    HTTP JSON

---
+++ src/main/java/com/xxg/xtoolkit/HttpUtil.java
@@ -0,0 +1,157 @@
+package com.xxg.xtoolkit;
+
+import okhttp3.*;
+import okio.BufferedSink;
+import okio.Okio;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+public class HttpUtil {
+
+    private static final OkHttpClient defaultOkHttpClient = new OkHttpClient();;
+
+    /**
+     * GET 请求
+     */
+    public String get(String url) throws IOException {
+        return get(url, null);
+    }
+
+    /**
+     * GET 请求（带参数）
+     */
+    public String get(String url, Map<String, String> queryParameters) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(url).newBuilder();
+        if (queryParameters != null) {
+            for (Map.Entry<String, String> queryParameter : queryParameters.entrySet()) {
+                urlBuilder.addQueryParameter(queryParameter.getKey(), queryParameter.getValue());
+            }
+        }
+        HttpUrl httpUrl = urlBuilder.build();
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .build();
+        try (Response response = defaultOkHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (Exception e) {
+            throw new IOException("请求异常，URL：" + httpUrl, e);
+        }
+    }
+
+    /**
+     * POST 请求（请求 BODY 为 form 表单）
+     */
+    public String postForm(String url, Map<String, String> formData) throws IOException {
+
+        FormBody.Builder requestBodyBuilder = new FormBody.Builder();
+        if (formData != null) {
+            for (Map.Entry<String, String> entry : formData.entrySet()) {
+                requestBodyBuilder.add(entry.getKey(), entry.getValue());
+            }
+        }
+        RequestBody body = requestBodyBuilder.build();
+
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .post(body);
+        Request request = builder.build();
+        try (Response response = defaultOkHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (Exception e) {
+            throw new IOException("请求异常，URL：" + url + "，RequestBody：" + formData, e);
+        }
+    }
+
+    /**
+     * POST 请求（请求 BODY 为 json，带 header）
+     */
+    public String postJson(String url, String requestBody, Map<String, String> requestHeaders) throws IOException {
+        RequestBody body = RequestBody.create(requestBody, MediaType.parse("application/json; charset=utf-8"));
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .post(body);
+        if (requestHeaders != null) {
+            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
+                builder.header(header.getKey(), header.getValue());
+            }
+        }
+        Request request = builder.build();
+        try (Response response = defaultOkHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (Exception e) {
+            throw new IOException("请求异常，URL：" + url + "，RequestBody：" + requestBody, e);
+        }
+    }
+
+    /**
+     * POST 请求（请求 BODY 为 json）
+     */
+    public String postJson(String url, String requestBody) throws IOException {
+        return postJson(url, requestBody, null);
+    }
+
+    /**
+     * GET 请求下载文件
+     */
+    public void downloadFile(String url, File localFile) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = defaultOkHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            try (BufferedSink sink = Okio.buffer(Okio.sink(localFile))) {
+                sink.writeAll(response.body().source());
+            }
+        } catch (Exception e) {
+            throw new IOException("下载文件异常，URL：" + url, e);
+        }
+    }
+
+    /**
+     * POST multipart/form-data 上传单个文件
+     */
+    public String uploadFile(String url, String fileParameterName, File file) throws IOException {
+        return uploadFile(url, null, Collections.singletonMap(fileParameterName, file), null);
+    }
+
+    /**
+     * POST multipart/form-data 上传文件
+     */
+    public String uploadFile(String url, Map<String, String> bodyParameters, Map<String, File> files, Map<String, String> requestHeaders) throws IOException {
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        if (bodyParameters != null) {
+            for (Map.Entry<String, String> bodyParameter : bodyParameters.entrySet()) {
+                bodyBuilder.addFormDataPart(bodyParameter.getKey(), bodyParameter.getValue());
+            }
+        }
+        if (files != null) {
+            for (Map.Entry<String, File> fileItem : files.entrySet()) {
+                bodyBuilder.addFormDataPart(fileItem.getKey(), fileItem.getValue().getName(),
+                        RequestBody.create(fileItem.getValue(), MediaType.parse("application/octet-stream")));
+            }
+        }
+        RequestBody body = bodyBuilder.build();
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .post(body);
+        if (requestHeaders != null) {
+            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
+                builder.header(header.getKey(), header.getValue());
+            }
+        }
+        Request request = builder.build();
+        try (Response response = defaultOkHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+        } catch (Exception e) {
+            throw new IOException("请求异常，URL：" + url, e);
+        }
+    }
+}
---
+++ src/main/java/com/xxg/xtoolkit/JsonUtil.java
@@ -0,0 +1,37 @@
+package com.xxg.xtoolkit;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
+
+public class JsonUtil {
+
+    private final static ObjectMapper COMMON_MAPPER;
+    private final static ObjectMapper SNAKE_CASE_MAPPER;
+
+    static {
+        COMMON_MAPPER = new ObjectMapper();
+        COMMON_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        SNAKE_CASE_MAPPER = new ObjectMapper();
+        SNAKE_CASE_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SNAKE_CASE_MAPPER.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    }
+
+    public static String toJSONString(Object obj) throws JsonProcessingException {
+        return COMMON_MAPPER.writeValueAsString(obj);
+    }
+
+    public static <T> T parseJSON(String json, Class<T> clazz) throws IOException {
+        return COMMON_MAPPER.readValue(json, clazz);
+    }
+
+    public static String toSnakeCaseJSONString(Object obj) throws JsonProcessingException {
+        return SNAKE_CASE_MAPPER.writeValueAsString(obj);
+    }
+
+    public static <T> T parseSnakeCaseJSON(String json, Class<T> clazz) throws IOException {
+        return SNAKE_CASE_MAPPER.readValue(json, clazz);
+    }
+}

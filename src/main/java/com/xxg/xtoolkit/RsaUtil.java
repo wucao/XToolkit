85ccaf6cdbb2735b129c73b8f2a2711f24b1cfa3
吴操 <wucao@qiyi.com>
2021-07-29 15:40:21 +0800

    证书支持

--- src/main/java/com/xxg/xtoolkit/RsaUtil.java
+++ src/main/java/com/xxg/xtoolkit/RsaUtil.java
@@ -4,16 +4,13 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -35,6 +32,16 @@ public class RsaUtil {
     public static byte[] encrypt(byte[] data, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] result = cipher.doFinal(data);
+        return result;
+    }
+
+    /**
+     * 公钥证书加密
+     */
+    public static byte[] encrypt(byte[] data, Certificate certificate) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, certificate);
         byte[] result = cipher.doFinal(data);
         return result;
     }
@@ -64,6 +71,13 @@ public class RsaUtil {
     }
 
     /**
+     * 公钥证书验证签名
+     */
+    public static boolean verify(byte[] data, byte[] sign, Certificate certificate) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+        return verify(data, sign, certificate, DEFAULT_SIGN_ALGORITHM);
+    }
+
+    /**
      * 私钥签名
      */
     public static byte[] sign(byte[] data, PrivateKey privateKey, String algorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
@@ -80,6 +94,17 @@ public class RsaUtil {
     public static boolean verify(byte[] data, byte[] sign, PublicKey publicKey, String algorithm) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
         Signature signature = Signature.getInstance(algorithm);
         signature.initVerify(publicKey);
+        signature.update(data);
+        return signature.verify(sign);
+    }
+
+
+    /**
+     * 公钥证书验证签名
+     */
+    public static boolean verify(byte[] data, byte[] sign, Certificate certificate, String algorithm) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+        Signature signature = Signature.getInstance(algorithm);
+        signature.initVerify(certificate);
         signature.update(data);
         return signature.verify(sign);
     }
@@ -143,9 +168,17 @@ public class RsaUtil {
      * 第一行一般是 -----BEGIN CERTIFICATE-----
      */
     public static PublicKey getPublicKeyFromCertificate(InputStream inputStream) throws CertificateException {
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(inputStream);
+        Certificate certificate = getCertificate(inputStream);
         return certificate.getPublicKey();
+    }
+
+    /**
+     * 从证书文件中获取证书
+     * 第一行一般是 -----BEGIN CERTIFICATE-----
+     */
+    public static Certificate getCertificate(InputStream inputStream) throws CertificateException {
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        return certificateFactory.generateCertificate(inputStream);
     }
 
     /**
@@ -159,32 +192,58 @@ public class RsaUtil {
     }
 
     /**
-     * 从 .p12/.pfx 证书中获取公钥
+     * 从证书文件中获取证书
+     * 第一行一般是 -----BEGIN CERTIFICATE-----
      */
-    public static PublicKey getPublicKeyFromPkcs12Certificate(InputStream inputStream, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+    public static Certificate getCertificate(File file) throws CertificateException, IOException {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            return getCertificate(inputStream);
+        }
+    }
+
+    /**
+     * 从 .p12/.pfx 证书中获取公钥证书
+     */
+    public static Certificate getCertificateFromPkcs12(InputStream inputStream, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(inputStream, password.toCharArray());
         Enumeration<String> aliases = keyStore.aliases();
         while (aliases.hasMoreElements()) {
             String alias = aliases.nextElement();
-            return keyStore.getCertificate(alias).getPublicKey();
+            return keyStore.getCertificate(alias);
         }
         throw new IOException("文件中不包含证书");
     }
 
     /**
+     * 从 .p12/.pfx 证书中获取公钥证书
+     */
+    public static Certificate getCertificateFromPkcs12(File file, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            return getCertificateFromPkcs12(inputStream, password);
+        }
+    }
+
+    /**
      * 从 .p12/.pfx 证书中获取公钥
      */
-    public static PublicKey getPublicKeyFromPkcs12Certificate(File file, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+    public static PublicKey getPublicKeyFromPkcs12(InputStream inputStream, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+        return getCertificateFromPkcs12(inputStream, password).getPublicKey();
+    }
+
+    /**
+     * 从 .p12/.pfx 证书中获取公钥
+     */
+    public static PublicKey getPublicKeyFromPkcs12(File file, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            return getPublicKeyFromPkcs12Certificate(inputStream, password);
+            return getPublicKeyFromPkcs12(inputStream, password);
         }
     }
 
     /**
      * 从 .p12/.pfx 证书中获取私钥
      */
-    public static PrivateKey getPrivateKeyFromPkcs12Certificate(InputStream inputStream, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
+    public static PrivateKey getPrivateKeyFromPkcs12(InputStream inputStream, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         keyStore.load(inputStream, password.toCharArray());
         Enumeration<String> aliases = keyStore.aliases();
@@ -198,9 +257,9 @@ public class RsaUtil {
     /**
      * 从 .p12/.pfx 证书中获取私钥
      */
-    public static PrivateKey getPrivateKeyFromPkcs12Certificate(File file, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
+    public static PrivateKey getPrivateKeyFromPkcs12(File file, String password) throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableKeyException {
         try (FileInputStream inputStream = new FileInputStream(file)) {
-            return getPrivateKeyFromPkcs12Certificate(inputStream, password);
+            return getPrivateKeyFromPkcs12(inputStream, password);
         }
     }
 }
